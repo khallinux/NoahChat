6f3a679d51aba228247a5a58923fd5be7eb002d1
khallinux <127399440+khallinux@users.noreply.github.com>
2023-03-10 01:28:21 +0100

    Update openai_response.py

--- openai_response.py
+++ openai_response.py
@@ -1,23 +1,5 @@
 from gtts import gTTS
 import subprocess
-import openai
-from api_key import API_KEY
-openai.api_key = API_KEY
-def generate_openai_response(text_input):
-    prompt = f" {text_input}"
-    print("Prompt:", prompt)
-    response = openai.Completion.create(
-        engine="text-davinci-003",
-        prompt=prompt,
-        temperature=0.3,
-        max_tokens=1000,
-        top_p=1,
-        frequency_penalty=0.2,
-        presence_penalty=0,
-    )
-    answer = response['choices'][0]['text']
-
-    return answer
 
 def text_to_speech(text):
     tts = gTTS(text=text, lang='en', tld='us')
